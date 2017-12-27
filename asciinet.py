2f7a41e155bdbde404c1d75dbb3a6c980c292804
Nariman Saftarli <nsaftarli@ryerson.ca>
2017-12-26 21:39:46 -0500

    working on the model now

--- asciinet.py
+++ asciinet.py
@@ -5,17 +5,45 @@ from keras import optimizers
 from keras import layers
 from keras import models
 from keras.applications import VGG16
+import imgdata
 
 
 
 char_array = ['M','N','H','Q','0','C','7','>','!',':','-',';','.',' ']
 
+(x_train, y_train) = imgdata.load_data()
+print(x_train.shape)
+print(y_train.shape)
 
-conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(300,300,3))
-conv_base.summary()
+x_test = x_train[:100]
+x_train = x_train[100:]
 
-conv_head = layers.Conv2D(1,(3,3),activation='relu')
+y_test = y_train[:100]
+y_train = y_train[100:]
+
+
+
+conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(218,178,3))
+# conv_base.summary()
+
+conv_head = layers.Conv2D(4,(3,3),activation='relu')
 
 model = models.Sequential()
 model.add(conv_base)
-model.add()
+model.add(conv_head)
+
+model.summary()
+
+model.compile(
+	loss='categorical_crossentropy',
+	optimizer=optimizers.SGD(lr=1e-2, momentum=0.9, decay=5e-4),
+	metrics=['accuracy'])
+
+history = model.fit(x_train,y_train, epochs=30)
+test_loss, test_acc = model.evaluate(x_test,y_test)
+
+print(test_loss)
+print(test_acc)
+
+
+
--- imgdata.py
+++ imgdata.py
@@ -1,18 +1,14 @@
 import os 
 import numpy as np 
-from scipy import misc
 from PIL import Image
-# import keras
-# import keras.preprocessing.text
-# import tensorflow as tf 
 
 #On Ubuntu
-#img_data_dir = '/home/nsaftarli/Documents/ascii-art/ASCIIArtNN/assets/rgb_input/img_align_celeba_png/'
-#ascii_data_dir = '/home/nsaftarli/Documents/ascii-art/ASCIIArtNN/assets/ascii_output/'
+img_data_dir = '/home/nsaftarli/Documents/ascii-art/ASCIIArtNN/assets/rgb_input/img_align_celeba_png/'
+ascii_data_dir = '/home/nsaftarli/Documents/ascii-art/ASCIIArtNN/assets/ascii_output/'
 
 #On OSX
-img_data_dir = '/Users/Nariman/Documents/GitHub/ASCIIArtNN/assets/rgb_in/'
-ascii_data_dir = '/Users/Nariman/Documents/GitHub/ASCIIArtNN/assets/ascii_out/'
+# img_data_dir = '/Users/Nariman/Documents/GitHub/ASCIIArtNN/assets/rgb_in/'
+# ascii_data_dir = '/Users/Nariman/Documents/GitHub/ASCIIArtNN/assets/ascii_out/'
 
 
 # images = np.zeros((202599,218,178,3), dtype='uint8')
@@ -24,27 +20,47 @@ char_dict = {'M':0,'N':1,'H':2,'Q':3,'$':4,'O':5,'C':6,'?':7,'7':8,'>':9,'!':10,
 Change this on beta. Data of different dimensions
 '''
 samples = 202533
-text_rows = 219
-text_cols = 350
+text_rows = 218
+text_cols = 178
 dims = 16
 
-x_train = np.zeros((samples,text_rows,text_cols,3), dtype='uint8')
-y_train = np.zeros((samples,text_rows*text_cols,dims), dtype='uint8')
+# x_train = np.zeros((samples,text_rows,text_cols,3), dtype='uint8')
+# y_train = np.zeros((samples,text_rows*text_cols,dims), dtype='uint8')
+
+test_batch_num = 1000
+
+x_train = np.zeros((test_batch_num,text_rows,text_cols,3), dtype='uint8')
+y_train = np.zeros((test_batch_num,text_rows * text_cols, dims), dtype='uint8')
 
 
 
-def load_data(image):
-	tstimg = img_data_dir + image
-	img = Image.open(tstimg)
-	print(img.size)
-	nparray = np.asarray(img,dtype='uint8')
-	print(nparray)
-	# tstimg = img_data_dir + 'in_0'
-	# face = misc.face()
-	# misc.imsave(tstimg,face)
-	# face = misc.imread('in_0')
-	# type(face)
-	# face.shape, face.dtype
+
+def load_images():
+	# tstimg = img_data_dir + image
+	# img = Image.open(tstimg)
+	# print("Image size is: " + str(img.size))
+	# nparray = np.asarray(img,dtype='uint8')
+	# print("Numpy array of image dims are: " + str(nparray.shape))
+
+	for i in range(test_batch_num):
+		imgpath = img_data_dir + 'in_' + str(i) + '.png'
+		img = Image.open(imgpath)
+		nparray = np.asarray(img,dtype='uint8')
+		x_train[i] = nparray
+
+	return x_train
+	# print(x_train.shape)
+	# print(x_train[80].shape)
+
+def load_labels():
+	for i in range(test_batch_num):
+		labelpath = 'in_' + str(i) + '.png'
+		indices = text_to_ints(labelpath).flatten()
+		o_h_indices = np.eye(dims)[indices]
+		y_train[i] = o_h_indices
+	# print(y_train[0].shape)
+	return y_train
+
 
 def text_to_ints(text):
 	textfile = open(ascii_data_dir + text)
@@ -55,18 +71,24 @@ def text_to_ints(text):
 				if char != '\n':
 					result[row,col] = char_dict[char]
 	textfile.close()
-	print(result)
+	# print(result)
 	return result
 
 	
 
-index_arr = text_to_ints('in_0')
-index_arr = index_arr.flatten()
-o_h_index_arr = np.eye(dims)[index_arr]
-print(o_h_index_arr.shape)
-print(o_h_index_arr)
-load_data('in_0.png')
+# index_arr = text_to_ints('in_0.png')
+# index_arr = index_arr.flatten()
+# o_h_index_arr = np.eye(dims)[index_arr]
+# print("One hot array of one sample is: " + str(o_h_index_arr.shape))
+# print(o_h_index_arr)
+# load_data('in_0.png')
 
+def load_data():
+	imgs = load_images()
+	# print(imgs.shape)
+	labels = load_labels()
+	# print(labels.shape)
+	return (imgs,labels)
 
 
 
