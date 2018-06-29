8423fa1d0e1f3622fdfa82ccf2aa8d8c6353fa09
Nariman Saftarli <nariman@pips.eecs.yorku.ca>
2018-06-29 19:19:59 -0400

    Removed colour pyramid

--- tfmodel.py
+++ tfmodel.py
@@ -159,16 +159,9 @@ class ASCIINet:
 
         ##############Colour Loss##################################################################################
         self.colour_map_in = self.get_avg_colour(self.input)
-        self.colour_map_in_d1 = self.blur_recombine(self.colour_map_in, w, stride=2)
-        self.colour_map_in_d2 = self.blur_recombine(self.colour_map_in_d1, w, stride=2)
-
         self.colour_map_out = self.get_avg_colour(self.view_output)
-        self.colour_map_out_d1 = self.blur_recombine(self.colour_map_out, w, stride=2)
-        self.colour_map_out_d2 = self.blur_recombine(self.colour_map_out_d1, w, stride=2)
 
-        self.colour_loss = 30 * (tf.losses.mean_squared_error(self.colour_map_in, self.colour_map_out) + \
-                           tf.losses.mean_squared_error(self.colour_map_in_d1, self.colour_map_out_d1) + \
-                           tf.losses.mean_squared_error(self.colour_map_in_d2, self.colour_map_out_d2))
+        self.colour_loss = 58 * (tf.losses.mean_squared_error(self.colour_map_in, self.colour_map_out))
         ###########################################################################################################
 
 
@@ -192,6 +185,8 @@ class ASCIINet:
         # tf.summary.image('downsampled_in', self.im1)
         # tf.summary.image('downsampled_out', self.im2)
         # tf.summary.image('blurred_out', self.blurred_out)
+        tf.summary.image('input_colour_map', self.colour_map_in)
+        tf.summary.image('output_colour_map', self.colour_map_out)
 
         tf.summary.scalar('entropy', self.entropy)
         tf.summary.scalar('variance', self.variance)
