2eacd53890130bcf3f20dcf3f8f4f9186a362ae9
Nariman Saftarli <nsaftarli@ryerson.ca>
2018-07-07 23:16:26 -0400

    added sigma hyperparam for GaussianBlurLayer

--- src/layers/GaussianBlurLayer.py
+++ src/layers/GaussianBlurLayer.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 from src.utils import gauss2d_kernel
 
 
-def GaussianBlurLayer(input, name, k_h, k_w, stride=1):
+def GaussianBlurLayer(input, name, k_h, k_w, stride=1, sigma=3):
     with tf.name_scope(name):
-        blur_kernel = gauss2d_kernel(shape=(k_h, k_w), sigma=3)
+        blur_kernel = gauss2d_kernel(shape=(k_h, k_w), sigma=sigma)
         blur_kernel = tf.reshape(tf.constant(blur_kernel, dtype=tf.float32),
                                  [k_h, k_w, 1, 1])
         blur_kernel = tf.tile(blur_kernel, [1, 1, 3, 1])
