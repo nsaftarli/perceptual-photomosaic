Fixed implementation of some of the normalization layers

src/layers/ConvLayer.py
@@ -12,9 +12,9 @@ def ConvLayer(input, name, out_channels, ksize=3, stride=1, activation='relu', t
         z = tf.nn.conv2d(input, w, strides=[1, stride, stride, 1], padding='SAME') + b
 
         if activation is not None:
-            z = GroupNormLayer(z, name + '/Group_Norm', G=1)
+            z = LayerNormLayer(z, name + '/Layer_Norm')
             z = tf.nn.relu(z)
         else:
-            z = GroupNormLayer(z, name + '/Group_Norm', G=1)
+            z = LayerNormLayer(z, name + '/Layer_Norm')
 
         return z

src/layers/NormalizationLayers.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 
 def InstanceNormLayer(input, name, epsilon=1e-12):
     with tf.name_scope(name):
-        mean, var = tf.nn.moments(input, axes=[1, 2, 3])
+        mean, var = tf.nn.moments(input, axes=[1, 2])
         return (input - mean) / (tf.sqrt(var+epsilon))
 
 
 def LayerNormLayer(input, name, eps=1e-12):
     with tf.name_scope(name):
-        mean, var = tf.nn.moments(input, axes=[0, 1, 2])
+        mean, var = tf.nn.moments(input, axes=[1, 2, 3])
         return (input-mean)/tf.sqrt(var+eps)
 
 